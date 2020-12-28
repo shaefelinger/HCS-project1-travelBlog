d67a5de5ce80d2421235bd899921d724ac5f55da
Steffen Häfelinger <s.haefelinger@gmx.de>
2020-12-28 09:33:55 +0100

    removed unused files

--- assets/unused/index.js
+++
@@ -1,9 +0,0 @@
-const burger = document.getElementById('burger');
-const sideMenu = document.getElementById('sideMenu');
-burger.addEventListener('click', onBurgerClick);
-sideMenu.addEventListener('click', onBurgerClick);
-
-function onBurgerClick() {
-  console.log('sdfsdf');
-  sideMenu.classList.toggle('sideMenu-hiding');
-}
--- todo.md
+++
@@ -1,8 +0,0 @@
-# TODO layout #
-* clean up
-* rest of blogpages
-* steffen links working
-
-
-
-original breakpoint @640 
