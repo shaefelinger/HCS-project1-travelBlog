cefc0760fc2da52fe374625c3768f48a0701aa46
Steffen Häfelinger <s.haefelinger@gmx.de>
2020-12-30 19:02:38 +0100

    added dirty addPost function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,7 @@
-/* location autocomplete for New Post-Page */
-function initialize() {
-  var options = {
-    types: ['(regions)'],
-  };
-  var input = document.getElementById('searchTextField');
-  var autocomplete = new google.maps.places.Autocomplete(input, options);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
-/* ================================ */
 const blogPosts = [
   {
     name: 'Tübingen, Germany',
-    coords: [48.5216364, 9.0576448],
-
+    coords: { lat: 48.5216364, lng: 9.0576448 },
     postTitle: 'The lovely City i used to live in',
     postDescription:
       'mir geht es gut, wie geht es dir? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! perferendis eaque, exercitationem praesentium nihil. Nulla! perferendis eaque, exercitationem praesentium nihil. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! perferendis eaque, exercitationem praesentium nihil. Nulla! perferendis eaque, exercitationem praesentium nihil Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! perferendis eaque, exercitationem praesentium nihil. Nulla! perferendis eaque, exercitationem praesentium nihil. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! perferendis eaque, exercitationem praesentium nihil. Nulla!Schluss',
@@ -22,13 +10,12 @@ const blogPosts = [
     postImage1URL:
       'https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sATtYBwJr07k53jQW9IvYkW1H-muAZc8BrLM7WbpBKxfr6jWqzl-gpXwlNCUpLdd4Hw2-2y6QxzwAMYyk6fBHecgFjwF07DWiPcxI9QI674x9iy5KKAwoRGgWRiKetr0DovD7A3--8PHf0TIjBvJu8RHRwzqmYJrPEolskNKaAmTaxvchkiaG&3u1599&5m1&2e1&callback=none&key=AIzaSyC6iru9XKYIvVQaPG6oK1sLFBXyeSJkwWs&token=67626',
     postImage2URL: 'https://picsum.photos/id/249/500',
-    postImage3URL: 'https://picsum.photos/id/240/500',
-    postImage4URL: 'https://picsum.photos/id/340/500',
     postAuthor: 'Steffen Häfelinger',
   },
   {
     name: 'Wilhelmshaven, Germany',
-    coords: [53.53234029999999, 8.1068722],
+    coords: { lat: 53.53234029999999, lng: 8.1068722 },
+
     postTitle: 'Schlicktown i used to live in',
     postDescription:
       'mir geht es nocht so gut, wie geht es dir? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia',
@@ -37,29 +24,11 @@ const blogPosts = [
     postImage1URL:
       'https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sATtYBwI2OePT8IOxK_qO1okxl-aY233Z1cL6RW3xgShigsuxKrtO2Vd6V-Sy_2igVEodwlXwq4fHNuoZR1tjEvBfUlwMmYR0VF2gxiWJZCNWvJ_q6U6aWzzFkGxJmIgWXy94sbF5n3x4IPN4IrRi8yzMNi5LGjFyeBudT-FU9E6e4Q0GOSpN&3u4208&5m1&2e1&callback=none&key=AIzaSyC6iru9XKYIvVQaPG6oK1sLFBXyeSJkwWs&token=49277',
     postImage2URL: 'https://picsum.photos/id/356/500',
-    postImage3URL: 'https://picsum.photos/id/351/500',
-    postImage4URL: 'https://picsum.photos/id/451/500',
-    postAuthor: 'Guest',
-  },
-  {
-    name: 'Wilhelmshaven, Germany',
-    coords: [53.53234029999999, 8.1068722],
-
-    postTitle: 'The ole Schlicktown i used to live in',
-    postDescription:
-      'mir geht es nocht so gut, wie geht es dir? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! perferendis eaque, exercitationem praesentium nihil',
-    rating: '3',
-    date: 'Visited in May 2019',
-    postImage1URL: 'https://picsum.photos/id/15/500',
-    postImage2URL: 'https://picsum.photos/id/353/500',
-    postImage3URL: 'https://picsum.photos/id/351/500',
-    postImage4URL: 'https://picsum.photos/id/451/500',
-
     postAuthor: 'Guest',
   },
   {
     name: 'Los Angeles, USA',
-    coords: [34.052235, -118.243683],
+    coords: { lat: 34.052235, lng: -118.243683 },
 
     postTitle: 'The ole Schlicktown i used to live in',
     postDescription:
@@ -68,13 +37,13 @@ const blogPosts = [
     date: '',
     postImage1URL: 'https://picsum.photos/id/600/500',
     postImage2URL: 'https://picsum.photos/id/151/500',
-    postImage3URL: 'https://picsum.photos/id/161/500',
-    postImage4URL: 'https://picsum.photos/id/161/500',
-
     postAuthor: 'Guest',
   },
 ];
 
+// localStorage.setItem('allLocations', JSON.stringify(blogPosts));
+
+const blogContainer = document.getElementById('blogContainer');
 /*
  ***** ALL POSTS AS OVERVIEW *****
  */
@@ -82,7 +51,11 @@ function printAllPosts() {
   // blogPosts.forEach((element) => {
   //   console.log(element.name);
   // });
-  blogPosts.forEach(addOnePost);
+  const allLocations = localStorage.getItem('allLocations');
+  allLocationsParsed = JSON.parse(allLocations);
+  // console.log(allLocationsParsed);
+  // blogPosts.forEach(addOnePost);
+  allLocationsParsed.forEach(addOnePost);
 }
 
 printAllPosts();
@@ -90,7 +63,7 @@ printAllPosts();
 /* ***** ADD SINGLE POSTS AS CARD TO DOM ***** */
 function addOnePost(element, index) {
   // console.log(element, index);
-  const blogContainer = document.getElementById('blogContainer');
+  // const blogContainer = document.getElementById('blogContainer');
   const newArticle = document.createElement('div');
   newArticle.classList.add('blogPost');
   // newArticle.setAttribute('id', `${index}`);
@@ -146,14 +119,16 @@ function addOnePost(element, index) {
 
 /* ===================== SINGLE POST-BLOGPAGE => RESULT  ===================== */
 function onClick(object) {
-  // console.log(object);
+  console.log(object);
   console.log(object.srcElement.id);
 
   // const id = object.path[0].id;
   const id = object.srcElement.id;
-  const element = blogPosts[id];
+  const array = JSON.parse(localStorage.getItem('allLocations'));
+  // const element = blogPosts[id];
+  const element = array[id];
 
-  const blogContainer = document.getElementById('blogContainer');
+  // const blogContainer = document.getElementById('blogContainer');
   blogContainer.innerHTML = '';
 
   const bannerImage = document.getElementById('bannerImage');
@@ -177,6 +152,7 @@ function onClick(object) {
  
 <div class="blogpageTextWrapper">
     <h3>${element.postTitle}</h3>
+    
     <div class="blogpageArticeImage" 
       style="background-image: url(${element.postImage2URL}); 
       width: 100%;
@@ -185,6 +161,7 @@ function onClick(object) {
       background-size: 100%;
       margin-bottom: 2rem">
     </div>
+
     <div class="authorPic largePic">
       <img src="../assets/jane_doe.jpg">
   </div>
@@ -196,8 +173,6 @@ function onClick(object) {
       <use xlink:href="#starRating${element.rating}">
     </svg>
     <p>${element.date}</p>
-
-  
 
     <p>${element.postDescription}</p>
 </div>
@@ -227,17 +202,6 @@ function onClick(object) {
 </section> */
   }
 
-  {
-    /* <div class="blogpageArticeImage" 
-  style="background-image: url(${element.postImage2URL}); 
-  width: 40%;
-  height: 400px;
-  background-repeat: no-repeat;
-  background-size: 100%;
-  float: right">
-</div> */
-  }
-
   blogContainer.appendChild(newArticle);
   // console.log(element.coords);
   initMap(element.coords);
@@ -255,7 +219,9 @@ function onClick(object) {
 function initMap(coords) {
   // The location of Uluru
   // console.log(coords);
-  const uluru = { lat: coords[0], lng: coords[1] };
+  // const uluru = { lat: coords[0], lng: coords[1] };
+  const uluru = coords;
+  // const uluru = '40.712775,-74.005973';
   // console.log(uluru);
   const options = {
     zoom: 6,
@@ -270,50 +236,93 @@ function initMap(coords) {
     position: uluru,
     map: map,
   });
-  // function addMarker(props) {
-  //   const marker = new google.maps.Marker({
-  //     position: props.coords,
-  //     map: map1,
-  //   });
 }
 
-// check for custom image
-// check content
-// if (props.content) {
-//   const infoWindow = new google.maps.InfoWindow({
-//     content: props.content,
-//   });
-//   marker.addListener('click', function () {
-//     infoWindow.open(map, marker);
-//   });
-// }
+// =================================================
 
-// array of markers
-// const markers = [
-//   {
-//     coords: { lat: 53.551086, lng: 9.993682 },
-//     iconImage:
-//       'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-//     content: '<h2>Hamburchh</h2>',
-//   },
-//   {
-//     coords: { lat: 54.551086, lng: 8.993682 },
-//     content: 'fick dich',
-//   },
-//   {
-//     coords: { lat: 51.509865, lng: -0.118092 },
-//   },
-//   {
-//     coords: { lat: 41.509865, lng: -2.118092 },
-//   },
-// ];
-// markers.forEach(addMarker);
+/* location autocomplete for New Post-Page */
+let currentPlace = 'currentPlace';
+function initialize() {
+  var options = {
+    types: ['(regions)'],
+  };
+  var input = document.getElementById('searchTextField');
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+  autocomplete.addListener('place_changed', () => {
+    // infowindow.close();
+    const place = autocomplete.getPlace();
+    // console.log(place);
+    currentPlace = place;
+    // if (!place.geometry) {
+    //   return;
+    // }
 
-/*
-  const infoWindow = new google.maps.InfoWindow({
-      content: '<h1>Hallo</h1',
+    // if (place.geometry.viewport) {
+    //   map.fitBounds(place.geometry.viewport);
+    // } else {
+    //   map.setCenter(place.geometry.location);
+    //   map.setZoom(17);
+    // }
+    // // Set the position of the marker using the place ID and location.
+    // marker.setPlace({
+    //   placeId: place.place_id,
+    //   location: place.geometry.location,
+    // });
+    // marker.setVisible(true);
+    // infowindowContent.children.namedItem('place-name').textContent = place.name;
+    // infowindowContent.children.namedItem('place-id').textContent =
+    //   place.place_id;
+    // infowindowContent.children.namedItem('place-address').textContent =
+    //   place.formatted_address;
+    // console.log(place);
+
+    // storeResult(place, input.value);
+    // printResult(place, input.value);
   });
-  marker.addListener('click', function () {
-      infoWindow.open(map, marker);
-  })
-  */
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+const addPostForm = document.getElementById('addPostForm');
+
+// alert(addPostForm);
+addPostForm.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  // console.log(currentPlace, titleField.value, descriptionField.value);
+  // const place = autocomplete.getPlace();
+  // console.log(place);
+  // const location = searchTextField.value;
+  // getInfoFromGoogleApi(location);
+  // getMap(location);
+  // userInputForm.reset();
+  // console.log(currentPlace);
+  const newEntry = {
+    name: currentPlace.formatted_address,
+    coords: currentPlace.geometry.location.toJSON(),
+    postTitle: titleField.value,
+    postDescription: descriptionField.value,
+    rating: ratingField.value,
+    date: 'Visited in May 2019',
+    postImage1URL: currentPlace.photos[0].getUrl(),
+    postImage2URL: currentPlace.photos[1].getUrl(),
+    postAuthor: 'Guest',
+  };
+
+  // const stringifiedNewEntry = JSON.stringify(newEntry);
+  // console.log(stringifiedNewEntry);
+  addToLocalStorage(newEntry);
+}
+
+function addToLocalStorage(newElement) {
+  let array = [];
+  array = JSON.parse(localStorage.getItem('allLocations'));
+  array.push(newElement);
+  // blogPosts = array;
+  // console.log(blogPosts);
+
+  localStorage.setItem('allLocations', JSON.stringify(array));
+  blogContainer.innerHTML = '';
+
+  printAllPosts();
+}
